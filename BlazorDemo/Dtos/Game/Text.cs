1fbc4009149d37f3163e34f24573dbccf07b70ec
Jeremy <jeremydaniel.bouvier@gmail.com>
2020-02-09 18:53:30 -0500

    work in progress

---
+++ BlazorDemo/Dtos/Game/Text.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorDemo.Dtos.Game
+{
+    public class Text : GameComponent
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Content { get; set; }
+        public string FontFamilly { get; set; }
+        public double FontSize { get; set; }
+        public string TextAnchor { get; set; }
+    }
+}
