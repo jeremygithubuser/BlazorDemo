cd6ddf41bda6300cde00a48f583a70df7632bb27
Jeremy <jeremydaniel.bouvier@gmail.com>
2020-02-09 13:32:42 -0500

    work in progress

--- BlazorDemo/Dtos/Game/GameComponent.cs
+++ BlazorDemo/Dtos/Game/GameComponent.cs
@@ -5,7 +5,9 @@ namespace BlazorDemo.Dtos.Game
     public abstract class GameComponent
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
         public string Stroke { get; set; }
+        public string Style { get; set; }
         public string Fill { get; set; }
     }
 }
